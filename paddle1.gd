51467bb0dfc9dca7d0ea1e53438d40d02f4c1945
Dmitrii Chizha <me@filaco.dev>
2026-05-13 11:44:19 +0300

    feat(paddle): add movement to paddle1

--- paddle1.gd
+++ paddle1.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
 
 @onready var paddle_radius = $CollisionShape2D.shape.radius
+@export var speed = 400 # How fast the player will move (pixels/sec).
 
 func _ready() -> void:
 	get_viewport().size_changed.connect(_on_view_port_resized)
 	_on_view_port_resized()
+	
+func _process(delta: float) -> void:
+	var direction = Input.get_axis("move_up", "move_down")
+	velocity.y = direction * speed
+
+	move_and_slide()
+	
+
 	
 func _on_view_port_resized() -> void:
 	var half_vp_width = get_viewport_rect().size.x / 2
